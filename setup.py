dfff31d1a646a8cd0d8ab85c1f3bd492fd3c2102
vaibhavthapli <hellovaibt@gmail.com>
2025-01-02 13:48:41 +0530

    update

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ HYPHEN_E_DOT = "-e ."
 
 
 def get_requirements() -> List[str]:
+    """
+    This Function will return list of requirements
+    """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
     requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
